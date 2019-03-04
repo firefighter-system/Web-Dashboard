ee806c58d48c14f31818d622b465f91886558f71
Yuhan Lee <ylee050@uottawa.ca>
2019-03-04 15:06:40 -0500

    testing serving index.html from public

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/api/getList', (req,res) => {
 
 // Serving React
 app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
   console.log("should be fine")
 });
 
