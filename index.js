dbd4f829f3ba4a47d3d58072b2ee0e44bce8236b
Yuhan Lee <ylee050@uottawa.ca>
2019-03-29 14:15:47 -0400

    added firebase db

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const dotenv = require('dotenv'); 
+dotenv.config(); 
 
 const app = express();
 
@@ -17,6 +19,33 @@ app.get('/api/getList', (req,res) => {
 app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+
+
+var firebase = require("firebase-admin"); 
+var serviceAccount = require("./serviceAccountKey.json");
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://firefightingmonitoringsystem.firebaseio.com/"
+});
+var db = firebase.database();
+var ref = db.ref("restricted_access/secret_document");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
+var usersRef = ref.child("users");
+usersRef.set({
+  alanisawesome: {
+    date_of_birth: "June 23, 1912",
+    full_name: "Alan Turing"
+  },
+  gracehop: {
+    date_of_birth: "December 9, 1906",
+    full_name: "Grace Hopper"
+  }
+});
+
 
 const port = process.env.PORT || 5000;
 app.listen(port);
