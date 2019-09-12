742ee36c61fccc7cf736178ccc36ed69d242213d
Yuhan Lee <ylee050@uottawa.ca>
2019-09-12 11:35:05 -0400

    updated file structures, now all routes are separatedD

---
+++ controllers/firebase.js
@@ -0,0 +1,19 @@
+var firebase = require("firebase-admin"); 
+var serviceAccount = require("../serviceAccountKey.json");
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://firefightingmonitoringsystem.firebaseio.com/"
+});
+
+var db = firebase.database();
+var ref = db.ref("restricted_access/secret_document");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
+module.exports.set = function(app) {
+  app.get('/firebase', (req,res) => {
+    console.log("in firebase")
+  });
+}
---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+var firebase = require('./firebase.js'); 
+
+module.exports.set = function(app) {
+  firebase.set(app); 
+}
