32288e1526ed20c1c5bfdb569ff419ef3a55918c
filipmatic <filip.matic.6@gmail.com>
2019-11-21 10:40:20 -0500

    Delete old files and comments; refactor components

--- client/src/App/components/ExternalPressure.jsx
+++ client/src/App/components/ExternalPressure.jsx
@@ -8,52 +8,9 @@ import CartesianGrid from 'recharts/lib/cartesian/CartesianGrid';
 import Tooltip from 'recharts/lib/component/Tooltip';
 import Legend from 'recharts/lib/component/Legend';
 
-import { WebSocketLink } from 'apollo-link-ws';
-import { PubSub } from 'graphql-subscriptions';
-import { SubscriptionClient } from "subscriptions-transport-ws";
-
 import firebaseConfig from "../firebase"
 
-// var database = firebaseConfig.database();
-
-// var ref1 = database.ref('pi_data/users/usr1');
-// ref1.on('value', data => {
-//   console.log(data.val())
-//   // handle the data here
-// })
-
-// var ref2 = database.ref('pi_data/users/usr2');
-// ref2.on('value', data => {
-//   console.log(data.val())
-//   // handle the data here
-// })
-
-// const wsLink = new WebSocketLink({
-//   uri: `ws://localhost:9000/`,
-//   options: {
-//     reconnect: true
-//   }
-// });
-
-// const GRAPHQL_ENDPOINT = "ws://localhost:9000/graphql";
-// const client = new SubscriptionClient(GRAPHQL_ENDPOINT, {
-//   reconnect: true
-// });
-
-// const link = new WebSocketLink(client);
-
-// const data = [ // y coordinate should be heart rate, x should be time, with multiple lines for the diff ppl
-//   { name: 'Mon', Visits: 2200, Orders: 3400 },
-//   { name: 'Tue', Visits: 1280, Orders: 2398 },
-//   { name: 'Wed', Visits: 5000, Orders: 4300 },
-//   { name: 'Thu', Visits: 4780, Orders: 2908 },
-//   { name: 'Fri', Visits: 5890, Orders: 4800 },
-//   { name: 'Sat', Visits: 4390, Orders: 3800 },
-//   { name: 'Sun', Visits: 4490, Orders: 4300 },
-// ];
-
-class ExPressure extends React.Component {
-// const HearRate = () => {
+class ExternalPressure extends React.Component {
 
   state = {
     user1_data: null,
@@ -68,7 +25,6 @@ class ExPressure extends React.Component {
     var ref1 = database.ref('pi_data/users/usr1');
     ref1.on('value', data => {
       console.log(data.val())
-      // state.state_data = data.val();
       this.setState({
         user1_data: data.val()
       })
@@ -76,7 +32,6 @@ class ExPressure extends React.Component {
 
     var ref2 = database.ref('pi_data/users/usr2');
     ref2.on('value', data => {
-      // console.log(data.val())
       this.setState({
         user2_data: data.val()
       })
@@ -84,7 +39,6 @@ class ExPressure extends React.Component {
 
     var ref3 = database.ref('pi_data/users/usr3');
     ref3.on('value', data => {
-      // console.log(data.val())
       this.setState({
         user3_data: data.val()
       })
@@ -92,33 +46,11 @@ class ExPressure extends React.Component {
 
     var ref4 = database.ref('pi_data/users/usr4');
     ref4.on('value', data => {
-      // console.log(data.val())
       this.setState({
         user4_data: data.val()
       })
     })
-
-    const wsLink = new WebSocketLink({
-      uri: `ws://localhost:9000/`,
-      options: {
-        reconnect: true
-      }
-    });
-  
-    const GRAPHQL_ENDPOINT = "ws://localhost:9000/graphql";
-    const client = new SubscriptionClient(GRAPHQL_ENDPOINT, {
-      reconnect: true
-    });
-  
-    const link = new WebSocketLink(client);
   }
-
-  // test = () => {
-  //   return this.state.state_data && [Object.keys(this.state.state_data)].map( (a) => {
-  //     console.log(a)
-  //     console.log('testingggg')
-  //   })
-  // }
   
   render() {
 
@@ -134,7 +66,7 @@ class ExPressure extends React.Component {
     let user_3_data = this.state.user3_data ? this.state.user3_data : date_object
     let user_4_data = this.state.user4_data ? this.state.user4_data : date_object
 
-    let data = [ // y coordinate should be heart rate, x should be time, with multiple lines for the diff ppl
+    let data = [ 
       { name: user_4_data[Object.keys(user_4_data)[Object.keys(user_4_data).length-7]] ? user_4_data[Object.keys(user_4_data)[Object.keys(user_4_data).length-7]].dateTime : '', User1: user_1_data[Object.keys(user_1_data)[Object.keys(user_1_data).length-7]] ? user_1_data[Object.keys(user_1_data)[Object.keys(user_1_data).length-7]].humidity : 0, User2: user_2_data[Object.keys(user_2_data)[Object.keys(user_2_data).length-7]] ? user_2_data[Object.keys(user_2_data)[Object.keys(user_2_data).length-7]].humidity : 0, User3: user_3_data[Object.keys(user_3_data)[Object.keys(user_3_data).length-7]] ? user_3_data[Object.keys(user_3_data)[Object.keys(user_3_data).length-7]].humidity : 0, User4: user_4_data[Object.keys(user_4_data)[Object.keys(user_4_data).length-7]] ? user_4_data[Object.keys(user_4_data)[Object.keys(user_4_data).length-7]].humidity : 0 },
       { name: user_4_data[Object.keys(user_4_data)[Object.keys(user_4_data).length-6]] ? user_4_data[Object.keys(user_4_data)[Object.keys(user_4_data).length-6]].dateTime : '', User1: user_1_data[Object.keys(user_1_data)[Object.keys(user_1_data).length-6]] ? user_1_data[Object.keys(user_1_data)[Object.keys(user_1_data).length-6]].humidity : 0, User2: user_2_data[Object.keys(user_2_data)[Object.keys(user_2_data).length-6]] ? user_2_data[Object.keys(user_2_data)[Object.keys(user_2_data).length-6]].humidity : 0, User3: user_3_data[Object.keys(user_3_data)[Object.keys(user_3_data).length-6]] ? user_3_data[Object.keys(user_3_data)[Object.keys(user_3_data).length-6]].humidity : 0, User4: user_4_data[Object.keys(user_4_data)[Object.keys(user_4_data).length-6]] ? user_4_data[Object.keys(user_4_data)[Object.keys(user_4_data).length-6]].humidity : 0 },
       { name: user_4_data[Object.keys(user_4_data)[Object.keys(user_4_data).length-5]] ? user_4_data[Object.keys(user_4_data)[Object.keys(user_4_data).length-5]].dateTime : '', User1: user_1_data[Object.keys(user_1_data)[Object.keys(user_1_data).length-5]] ? user_1_data[Object.keys(user_1_data)[Object.keys(user_1_data).length-5]].humidity : 0, User2: user_2_data[Object.keys(user_2_data)[Object.keys(user_2_data).length-5]] ? user_2_data[Object.keys(user_2_data)[Object.keys(user_2_data).length-5]].humidity : 0, User3: user_3_data[Object.keys(user_3_data)[Object.keys(user_3_data).length-5]] ? user_3_data[Object.keys(user_3_data)[Object.keys(user_3_data).length-5]].humidity : 0, User4: user_4_data[Object.keys(user_4_data)[Object.keys(user_4_data).length-5]] ? user_4_data[Object.keys(user_4_data)[Object.keys(user_4_data).length-5]].humidity : 0 },
@@ -165,21 +97,4 @@ class ExPressure extends React.Component {
   }
 }
 
-// function HearRate() {
-//   return (
-//     // 99% per https://github.com/recharts/recharts/issues/172
-//     <ResponsiveContainer width="99%" height={320}>
-//       <LineChart data={data}>
-//         <XAxis dataKey="name" />
-//         <YAxis />
-//         <CartesianGrid vertical={false} strokeDasharray="3 3" />
-//         <Tooltip />
-//         <Legend />
-//         <Line type="monotone" dataKey="Visits" stroke="#82ca9d" />
-//         <Line type="monotone" dataKey="Orders" stroke="#8884d8" activeDot={{ r: 8 }} />
-//       </LineChart>
-//     </ResponsiveContainer>
-//   );
-// } user_4_data[Object.keys(user_4_data)[Object.keys(user_4_data).length-7]] ? user_4_data[Object.keys(user_4_data)[Object.keys(user_4_data).length-7]] : 0
-
-export default ExPressure;
+export default ExternalPressure;
